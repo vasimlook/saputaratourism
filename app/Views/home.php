2ad646e8b2300a9a8e9cb519bed17bf4e3d0177f
likevishal <ivishalonline@gmail.com>
2022-04-13 17:46:22 +0530

    Update home page

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,216 +1,89 @@
-<div class="container margin_60_35">
-			<div class="main_title_3">
-				<span></span>
-				<h2>Famous Shops</h2>
-				<p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
-				<a href="<?php echo LISTING_LINK.'0' ?>">See all</a>
-			</div>
-			<div class="row add_bottom_30">
-				<div class="col-lg-3 col-sm-6">
-					<a href="#" class="grid_item small">
+<div class="bg_color_1">
+			<div class="container margin_80_55">
+				<div class="main_title_2">
+					<span><em></em></span>
+					<h2>Popular Categories</h2>
+					<p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
+				</div>
+				<div class="row">
+				<div class="col-lg-4 col-sm-6">
+					<a href="grid-listings-filterstop.html" class="grid_item">
 						<figure>
-							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/shop_1.jpg" alt="">
+							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/home_cat_1.jpg" alt="">
 							<div class="info">
-								<h3>Victoria Secretes</h3>
+								<small>122 Locations</small>
+								<h3>Shops</h3>
 							</div>
 						</figure>
 					</a>
 				</div>
-				<div class="col-lg-3 col-sm-6">
-					<a href="#" class="grid_item small">
+				<!-- /grid_item -->
+				<div class="col-lg-4 col-sm-6">
+					<a href="grid-listings-filterstop.html" class="grid_item">
 						<figure>
-							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/shop_2.jpg" alt="">
+							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/home_cat_2.jpg" alt="">
 							<div class="info">
-								<h3>Louis Vuitton</h3>
+								<small>245 Locations</small>
+								<h3>Hotels</h3>
 							</div>
 						</figure>
 					</a>
 				</div>
-				<div class="col-lg-3 col-sm-6">
-					<a href="detail-shop.html" class="grid_item small">
+				<!-- /grid_item -->
+				<div class="col-lg-4 col-sm-6">
+					<a href="grid-listings-filterstop.html" class="grid_item">
 						<figure>
-							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/shop_3.jpg" alt="">
+							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/home_cat_3.jpg" alt="">
 							<div class="info">
-								<h3>Burberry</h3>
+								<small>95 Locations</small>
+								<h3>Restaurants</h3>
 							</div>
 						</figure>
 					</a>
 				</div>
-				<div class="col-lg-3 col-sm-6">
-					<a href="detail-shop.html" class="grid_item small">
+				<!-- /grid_item -->
+				<div class="col-lg-4 col-sm-6">
+					<a href="grid-listings-filterstop.html" class="grid_item">
 						<figure>
-							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/shop_4.jpg" alt="">
+							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/home_cat_4.jpg" alt="">
 							<div class="info">
-								<h3>Pinko</h3>
+								<small>123 Locations</small>
+								<h3>Bars</h3>
 							</div>
 						</figure>
 					</a>
 				</div>
-			</div>
-			<!-- /row -->
-			
-			<div class="main_title_3">
-				<span></span>
-				<h2>Popular Hotels</h2>
-				<p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
-				<a href="<?php echo LISTING_LINK.'0' ?>">See all</a>
-			</div>
-			<div class="row add_bottom_30">
-				<div class="col-lg-3 col-sm-6">
-					<a href="detail-hotel.html" class="grid_item small">
+				<!-- /grid_item -->
+				<div class="col-lg-4 col-sm-6">
+					<a href="grid-listings-filterstop.html" class="grid_item">
 						<figure>
-							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/hotel_1.jpg" alt="">
+							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/home_cat_5.jpg" alt="">
 							<div class="info">
-								<div class="cat_star"><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i></div>
-								<h3>Hotel Mariott</h3>
+								<small>92 Locations</small>
+								<h3>Events</h3>
 							</div>
 						</figure>
 					</a>
 				</div>
-				<div class="col-lg-3 col-sm-6">
-					<a href="detail-hotel.html" class="grid_item small">
+				<!-- /grid_item -->
+				<div class="col-lg-4 col-sm-6">
+					<a href="grid-listings-filterstop.html" class="grid_item">
 						<figure>
-							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/hotel_2.jpg" alt="">
+							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/home_cat_6.jpg" alt="">
 							<div class="info">
-								<div class="cat_star"><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i></div>
-								<h3>Hotel Concorde</h3>
+								<small>92 Locations</small>
+								<h3>Fitness</h3>
 							</div>
 						</figure>
 					</a>
 				</div>
-				<div class="col-lg-3 col-sm-6">
-					<a href="detail-hotel.html" class="grid_item small">
-						<figure>
-							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/hotel_3.jpg" alt="">
-							<div class="info">
-								<div class="cat_star"><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i></div>
-								<h3>Hotel La Defanse</h3>
-							</div>
-						</figure>
-					</a>
-				</div>
-				<div class="col-lg-3 col-sm-6">
-					<a href="detail-hotel.html" class="grid_item small">
-						<figure>
-							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/hotel_4.jpg" alt="">
-							<div class="info">
-								<div class="cat_star"><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i></div>
-								<h3>Hotel Carlton</h3>
-							</div>
-						</figure>
-					</a>
-				</div>
+				<!-- /grid_item -->
 			</div>
 			<!-- /row -->
-			
-			<div class="main_title_3">
-				<span></span>
-				<h2>Top Restaurants</h2>
-				<p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
-				<a href="<?php echo LISTING_LINK.'0' ?>">See all</a>
 			</div>
-			<div class="row ">
-				<div class="col-lg-3 col-sm-6">
-					<a href="detail-restaurant.html" class="grid_item small">
-						<figure>
-							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/restaurant_1.jpg" alt="">
-							<div class="info">
-								<h3>Da Alfredo</h3>
-							</div>
-						</figure>
-					</a>
-				</div>
-				<div class="col-lg-3 col-sm-6">
-					<a href="detail-restaurant.html" class="grid_item small">
-						<figure>
-							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/restaurant_2.jpg" alt="">
-							<div class="info">
-								<h3>Bistroter</h3>
-							</div>
-						</figure>
-					</a>
-				</div>
-				<div class="col-lg-3 col-sm-6">
-					<a href="detail-restaurant.html" class="grid_item small">
-						<figure>
-							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/restaurant_3.jpg" alt="">
-							<div class="info">
-								<h3>Da Luigi</h3>
-							</div>
-						</figure>
-					</a>
-				</div>
-				<div class="col-lg-3 col-sm-6">
-					<a href="detail-restaurant.html" class="grid_item small">
-						<figure>
-							<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/restaurant_4.jpg" alt="">
-							<div class="info">
-								<h3>Marco King</h3>
-							</div>
-						</figure>
-					</a>
-				</div>
-			</div>
-			<!-- /row -->
+			<!-- /container -->	
 		</div>
-		<!-- /container -->
-		
-		<div class="call_section">
-			<div class="wrapper">
-				<div class="container margin_80_55">
-					<div class="main_title_2">
-						<span><em></em></span>
-						<h2>How it Works</h2>
-						<p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
-					</div>
-					<div class="row">
-						<div class="col-md-4">
-							<div class="box_how">
-								<i class="pe-7s-search"></i>
-								<h3>Search Locations</h3>
-								<p>An nec placerat repudiare scripserit, temporibus complectitur at sea, vel ignota fierent eloquentiam id.</p>
-								<span></span>
-							</div>
-						</div>
-						<div class="col-md-4">
-							<div class="box_how">
-								<i class="pe-7s-info"></i>
-								<h3>View Location Info</h3>
-								<p>An nec placerat repudiare scripserit, temporibus complectitur at sea, vel ignota fierent eloquentiam id.</p>
-								<span></span>
-							</div>
-						</div>
-						<div class="col-md-4">
-							<div class="box_how">
-								<i class="pe-7s-like2"></i>
-								<h3>Book, Reach or Call</h3>
-								<p>An nec placerat repudiare scripserit, temporibus complectitur at sea, vel ignota fierent eloquentiam id.</p>
-							</div>
-						</div>
-					</div>
-					<!-- /row -->
-					<p class="text-center add_top_30 wow bounceIn" data-wow-delay="0.5s"><a href="account.html" class="btn_1 rounded">Register Now</a></p>
-				</div>
-				<canvas id="hero-canvas" width="1920" height="1080"></canvas>
-			</div>
-			<!-- /wrapper -->
-		</div>
-		<!--/call_section-->
-		
-		<div class="container margin_80_55">
-			<div class="main_title_2">
-				<span><em></em></span>
-				<h2>Sparker App Available</h2>
-				<p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
-			</div>
-			<div class="row justify-content-center text-center">
-				<div class="col-md-6">
-					<img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/mobile.svg" alt="" class="img-fluid add_bottom_45">
-					<div class="app_icons">
-						<a href="#0" class="pr-lg-2"><img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/app_android.svg" alt=""></a>
-						<a href="#0" class="pl-lg-2"><img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/app_apple.svg" alt=""></a>
-					</div>
-					<div class="add_bottom_15"><small>*An eum dolores tractatos, aeterno menandri deseruisse ut eum.</small></div>
-				</div>
-			</div>
-		</div>
+		<!-- /bg_color_1 -->
+
+	
+
