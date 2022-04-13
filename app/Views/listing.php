b814b4d0578f6a2021b9804b7a06bba076810e62
likevishal <ivishalonline@gmail.com>
2022-04-13 17:57:01 +0530

    Update listing page

--- app/Views/listing.php
+++ app/Views/listing.php
@@ -1,230 +1,125 @@
 <div class="container margin_60_35">
-			<div class="row">
-				<aside class="col-lg-3" id="sidebar">
-					<div id="filters_col">
-						<a data-toggle="collapse" href="#collapseFilters" aria-expanded="false" aria-controls="collapseFilters" id="filters_col_bt">Filters </a>
-						<div class="collapse show" id="collapseFilters">
-							<div class="filter_type">
-								<h6>Category</h6>
-								<ul>
-									<li>
-										<label class="container_check">Restaurants <small>43</small>
-										  <input type="checkbox">
-										  <span class="checkmark"></span>
-										</label>
-									</li>
-									<li>
-										<label class="container_check">Shops <small>33</small>
-										  <input type="checkbox">
-										  <span class="checkmark"></span>
-										</label>
-									</li>
-									<li>
-										<label class="container_check">Bars <small>12</small>
-										  <input type="checkbox">
-										  <span class="checkmark"></span>
-										</label>
-									</li>
-									<li>
-										<label class="container_check">Events <small>44</small>
-										  <input type="checkbox">
-										  <span class="checkmark"></span>
-										</label>
-									</li>
-								</ul>
-							</div>
-							<div class="filter_type">
-                                <h6>Distance</h6>
-                                <div class="distance"> Radius around selected destination <span></span> km</div>
-								<input type="range" min="10" max="100" step="10" value="30" data-orientation="horizontal">
-                            </div>
-							<div class="filter_type">
-								<h6>Rating</h6>
-								<ul>
-									<li>
-										<label class="container_check">Superb 9+ <small>34</small>
-										  <input type="checkbox">
-										  <span class="checkmark"></span>
-										</label>
-									</li>
-									<li>
-										<label class="container_check">Very Good 8+ <small>21</small>
-										  <input type="checkbox">
-										  <span class="checkmark"></span>
-										</label>
-									</li>
-									<li>
-										<label class="container_check">Good 7+ <small>15</small>
-										  <input type="checkbox">
-										  <span class="checkmark"></span>
-										</label>
-									</li>
-									<li>
-										<label class="container_check">Pleasant 6+ <small>34</small>
-										  <input type="checkbox">
-										  <span class="checkmark"></span>
-										</label>
-									</li>
-								</ul>
-							</div>
-						</div>
-						<!--/collapse -->
+			<div class="strip list_view">
+				<div class="row no-gutters">
+					<div class="col-lg-5">
+						<figure>
+							<a href="<?= DETAILS_LINK.'0' ?>"><img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/location_1.jpg" class="img-fluid" alt=""><div class="read_more"><span>Read more</span></div></a>
+							<small>Bar</small>
+						</figure>
 					</div>
-					<!--/filters col-->
-				</aside>
-				<!-- /aside -->
-
-				<div class="col-lg-9">
-					<div class="row">
-						<div class="col-md-6">
-							<div class="strip grid">
-								<figure>
-									<a href="#0" class="wish_bt"></a>
-									<a href="<?php echo DETAILS_LINK.'0' ?>"><img src="img/location_1.jpg" class="img-fluid" alt="">
-										<div class="read_more"><span>Read more</span></div>
-									</a>
-									<small>Restaurant</small>
-								</figure>
-								<div class="wrapper">
-									<h3><a href="<?php echo DETAILS_LINK.'0' ?>">Da Alfredo</a></h3>
-									<small>27 Old Gloucester St</small>
-									<p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu.</p>
-									<a class="address" href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x47e66e1de36f4147:0xb6615b4092e0351f!2sAssistance+Publique+-+H%C3%B4pitaux+de+Paris+(AP-HP)+-+Si%C3%A8ge!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361">Get directions</a>
-								</div>
-								<ul>
-									<li><span class="loc_open">Now Open</span></li>
-									<li>
-										<div class="score"><span>Superb<em>350 Reviews</em></span><strong>8.9</strong></div>
-									</li>
-								</ul>
-							</div>
+					<div class="col-lg-7">
+						<div class="wrapper">
+							<a href="#0" class="wish_bt"></a>
+							<h3><a href="<?= DETAILS_LINK.'0' ?>">Da Alfredo</a></h3>
+							<small>27 Old Gloucester St</small>
+							<p>Dicam diceret ut ius, no epicuri dissentiet philosophia vix. Id usu zril tacimates neglegentur. Eam id legimus torquatos cotidieque, usu decore percipitur definitiones ex, nihil utinam recusabo mel no.</p>
+							<a class="address" href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x47e66e1de36f4147:0xb6615b4092e0351f!2sAssistance+Publique+-+H%C3%B4pitaux+de+Paris+(AP-HP)+-+Si%C3%A8ge!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361">Get directions</a>
 						</div>
-						<!-- /strip grid -->
-						<div class="col-md-6">
-							<div class="strip grid">
-								<figure>
-									<a href="#0" class="wish_bt"></a>
-									<a href="<?php echo DETAILS_LINK.'0' ?>"><img src="img/location_2.jpg" class="img-fluid" alt="">
-										<div class="read_more"><span>Read more</span></div>
-									</a>
-									<small>Bar</small>
-								</figure>
-								<div class="wrapper">
-									<h3><a href="<?php echo DETAILS_LINK.'0' ?>">Limon Bar</a></h3>
-									<small>438 Rush Green Road, Romford</small>
-									<p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu.</p>
-									<a class="address" href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x47e66e1de36f4147:0xb6615b4092e0351f!2sAssistance+Publique+-+H%C3%B4pitaux+de+Paris+(AP-HP)+-+Si%C3%A8ge!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361">Get directions</a>
-								</div>
-								<ul>
-									<li><span class="loc_open">Now Open</span></li>
-									<li>
-										<div class="score"><span>Good<em>350 Reviews</em></span><strong>7.0</strong></div>
-									</li>
-								</ul>
-							</div>
-						</div>
-						<!-- /strip grid -->
-						<div class="col-md-6">
-							<div class="strip grid">
-								<figure>
-									<a href="#0" class="wish_bt"></a>
-									<a href="<?php echo DETAILS_LINK.'0' ?>"><img src="img/location_3.jpg" class="img-fluid" alt="">
-										<div class="read_more"><span>Read more</span></div>
-									</a>
-									<small>Shop</small>
-								</figure>
-								<div class="wrapper">
-									<h3><a href="<?php echo DETAILS_LINK.'0' ?>">Mary Boutique</a></h3>
-									<small>43 Stephen Road, Bexleyheath</small>
-									<p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu.</p>
-									<a class="address" href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x47e66e1de36f4147:0xb6615b4092e0351f!2sAssistance+Publique+-+H%C3%B4pitaux+de+Paris+(AP-HP)+-+Si%C3%A8ge!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361">Get directions</a>
-								</div>
-								<ul>
-									<li><span class="loc_closed">Now Closed</span></li>
-									<li>
-										<div class="score"><span>Good<em>350 Reviews</em></span><strong>7.0</strong></div>
-									</li>
-								</ul>
-							</div>
-						</div>
-						<!-- /strip grid -->
-						<div class="col-md-6">
-							<div class="strip grid">
-								<figure>
-									<a href="#0" class="wish_bt"></a>
-									<a href="<?php echo DETAILS_LINK.'0' ?>"><img src="img/location_4.jpg" class="img-fluid" alt="">
-										<div class="read_more"><span>Read more</span></div>
-									</a>
-									<small>Bar</small>
-								</figure>
-								<div class="wrapper">
-									<h3><a href="<?php echo DETAILS_LINK.'0' ?>">Garden Bar</a></h3>
-									<small>40 Beechwood Road, Sanderstead</small>
-									<p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu.</p>
-									<a class="address" href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x47e66e1de36f4147:0xb6615b4092e0351f!2sAssistance+Publique+-+H%C3%B4pitaux+de+Paris+(AP-HP)+-+Si%C3%A8ge!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361">Get directions</a>
-								</div>
-								<ul>
-									<li><span class="loc_closed">Now Closed</span></li>
-									<li>
-										<div class="score"><span>Superb<em>350 Reviews</em></span><strong>9.0</strong></div>
-									</li>
-								</ul>
-							</div>
-						</div>
-						<!-- /strip grid -->
-						<div class="col-md-6">
-							<div class="strip grid">
-								<figure>
-									<a href="#0" class="wish_bt"></a>
-									<a href="<?php echo DETAILS_LINK.'0' ?>"><img src="img/location_5.jpg" class="img-fluid" alt="">
-										<div class="read_more"><span>Read more</span></div>
-									</a>
-									<small>Hotel</small>
-								</figure>
-								<div class="wrapper">
-									<h3><a href="<?php echo DETAILS_LINK.'0' ?>">Mariott Hotel</a></h3>
-									<small>213 Malden Road, New Malden</small>
-									<p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu.</p>
-									<a class="address" href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x47e66e1de36f4147:0xb6615b4092e0351f!2sAssistance+Publique+-+H%C3%B4pitaux+de+Paris+(AP-HP)+-+Si%C3%A8ge!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361">Get directions</a>
-								</div>
-								<ul>
-									<li><span class="loc_open">Now Open</span></li>
-									<li>
-										<div class="score"><span>Good<em>350 Reviews</em></span><strong>7.5</strong></div>
-									</li>
-								</ul>
-							</div>
-						</div>
-						<!-- /strip grid -->
-						<div class="col-md-6">
-							<div class="strip grid">
-								<figure>
-									<a href="#0" class="wish_bt"></a>
-									<a href="<?php echo DETAILS_LINK.'0' ?>"><img src="img/location_6.jpg" class="img-fluid" alt="">
-										<div class="read_more"><span>Read more</span></div>
-									</a>
-									<small>Event</small>
-								</figure>
-								<div class="wrapper">
-									<h3><a href="<?php echo DETAILS_LINK.'0' ?>">Six Pistols</a></h3>
-									<small>Coverdale Road, Willesden</small>
-									<p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu.</p>
-									<a class="address" href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x47e66e1de36f4147:0xb6615b4092e0351f!2sAssistance+Publique+-+H%C3%B4pitaux+de+Paris+(AP-HP)+-+Si%C3%A8ge!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361">Get directions</a>
-								</div>
-								<ul>
-									<li><span class="loc_open">Now Open</span></li>
-									<li>
-										<div class="score"><span>Good<em>350 Reviews</em></span><strong>7.8</strong></div>
-									</li>
-								</ul>
-							</div>
-						</div>
-						<!-- /strip grid -->
+						<ul>
+							<li><span class="loc_open">Now Open</span></li>
+							<li><div class="score"><span>Superb<em>350 Reviews</em></span><strong>8.9</strong></div></li>
+						</ul>
 					</div>
-					<!-- /row -->
-
-					<p class="text-center"><a href="#0" class="btn_1 rounded add_top_30">Load more</a></p>
 				</div>
-				<!-- /col -->
-			</div>		
+			</div>
+			<!-- /strip list_view -->
+			<div class="strip list_view">
+				<div class="row no-gutters">
+					<div class="col-lg-5">
+						<figure>
+							<a href="<?= DETAILS_LINK.'0' ?>"><img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/location_2.jpg" class="img-fluid" alt=""><div class="read_more"><span>Read more</span></div></a>
+							<small>Bar</small>
+						</figure>
+					</div>
+					<div class="col-lg-7">
+						<div class="wrapper">
+							<a href="#0" class="wish_bt"></a>
+							<h3><a href="<?= DETAILS_LINK.'0' ?>">Limon Bar</a></h3>
+							<small>438 Rush Green Road, Romford</small>
+							<p>Dicam diceret ut ius, no epicuri dissentiet philosophia vix. Id usu zril tacimates neglegentur. Eam id legimus torquatos cotidieque, usu decore percipitur definitiones ex, nihil utinam recusabo mel no.</p>
+							<a class="address" href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x47e66e1de36f4147:0xb6615b4092e0351f!2sAssistance+Publique+-+H%C3%B4pitaux+de+Paris+(AP-HP)+-+Si%C3%A8ge!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361">Get directions</a>
+						</div>
+						<ul>
+							<li><span class="loc_open">Now Open</span></li>
+							<li><div class="score"><span>Good<em>350 Reviews</em></span><strong>7.0</strong></div></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<!-- /strip list_view -->
+			<div class="strip list_view">
+				<div class="row no-gutters">
+					<div class="col-lg-5">
+						<figure>
+							<a href="<?= DETAILS_LINK.'0' ?>"><img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/location_3.jpg" class="img-fluid" alt=""><div class="read_more"><span>Read more</span></div></a>
+							<small>Shop</small>
+						</figure>
+					</div>
+					<div class="col-lg-7">
+						<div class="wrapper">
+							<a href="#0" class="wish_bt"></a>
+							<h3><a href="<?= DETAILS_LINK.'0' ?>">Mary Boutique</a></h3>
+							<small>43 Stephen Road, Bexleyheath</small>
+							<p>Dicam diceret ut ius, no epicuri dissentiet philosophia vix. Id usu zril tacimates neglegentur. Eam id legimus torquatos cotidieque, usu decore percipitur definitiones ex, nihil utinam recusabo mel no.</p>
+							<a class="address" href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x47e66e1de36f4147:0xb6615b4092e0351f!2sAssistance+Publique+-+H%C3%B4pitaux+de+Paris+(AP-HP)+-+Si%C3%A8ge!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361">Get directions</a>
+						</div>
+						<ul>
+							<li><span class="loc_closed">Now Closed</span></li>
+							<li><div class="score"><span>Good<em>350 Reviews</em></span><strong>7.0</strong></div></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<!-- /strip list_view -->
+			<div class="strip list_view">
+				<div class="row no-gutters">
+					<div class="col-lg-5">
+						<figure>
+							<a href="<?= DETAILS_LINK.'0' ?>"><img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/location_4.jpg" class="img-fluid" alt=""><div class="read_more"><span>Read more</span></div></a>
+							<small>Bar</small>
+						</figure>
+					</div>
+					<div class="col-lg-7">
+						<div class="wrapper">
+							<a href="#0" class="wish_bt"></a>
+							<h3><a href="<?= DETAILS_LINK.'0' ?>">Garden Bar</a></h3>
+							<small>40 Beechwood Road, Sanderstead</small>
+							<p>Dicam diceret ut ius, no epicuri dissentiet philosophia vix. Id usu zril tacimates neglegentur. Eam id legimus torquatos cotidieque, usu decore percipitur definitiones ex, nihil utinam recusabo mel no.</p>
+							<a class="address" href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x47e66e1de36f4147:0xb6615b4092e0351f!2sAssistance+Publique+-+H%C3%B4pitaux+de+Paris+(AP-HP)+-+Si%C3%A8ge!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361">Get directions</a>
+						</div>
+						<ul>
+							<li><span class="loc_closed">Now Closed</span></li>
+							<li><div class="score"><span>Superb<em>350 Reviews</em></span><strong>9.0</strong></div></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<!-- /strip list_view -->
+			
+			<div class="strip list_view">
+				<div class="row no-gutters">
+					<div class="col-lg-5">
+						<figure>
+							<a href="<?= DETAILS_LINK.'0' ?>"><img src="<?php echo FRONT_ASSETS_FOLDER; ?>img/location_5.jpg" class="img-fluid" alt=""><div class="read_more"><span>Read more</span></div></a>
+							<small>Hotel</small>
+						</figure>
+					</div>
+					<div class="col-lg-7">
+						<div class="wrapper">
+							<a href="#0" class="wish_bt"></a>
+							<h3><a href="<?= DETAILS_LINK.'0' ?>">Mariott Hotel</a></h3>
+							<small>213 Malden Road, New Malden</small>
+							<p>Dicam diceret ut ius, no epicuri dissentiet philosophia vix. Id usu zril tacimates neglegentur. Eam id legimus torquatos cotidieque, usu decore percipitur definitiones ex, nihil utinam recusabo mel no.</p>
+							<a class="address" href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x47e66e1de36f4147:0xb6615b4092e0351f!2sAssistance+Publique+-+H%C3%B4pitaux+de+Paris+(AP-HP)+-+Si%C3%A8ge!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361">Get directions</a>
+						</div>
+						<ul>
+							<li><span class="loc_open">Now Open</span></li>
+							<li><div class="score"><span>Good<em>350 Reviews</em></span><strong>7.5</strong></div></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<!-- /strip list_view -->		
+			
+		
 		</div>
